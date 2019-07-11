eb7a4ad802bd642a4fe4cd81ca445a717d297e27
Dor Fire <dorfire@microsoft.com>
2019-07-11 12:09:57 -0700

    Added timeout tester

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ Sample app that run `pip install` in various directories to measure package down
 '''
 import os, subprocess, shutil
 from os import path
+from time import sleep
 from flask import Flask, Response
 
 
@@ -11,6 +12,9 @@ APP_DIR = path.dirname(path.abspath(__file__))
 PIP_ROOT_DIR = '/tmp/pip'
 REQSET_DIR = path.join(APP_DIR, 'reqsets')
 REQSET_EXT = '.txt'
+
+def res(body):
+	return Response(body, mimetype='text/plain')
 
 
 def isreqset(filename):
@@ -26,7 +30,12 @@ def index():
 	body = 'Navigate to /pipinstall/<reqset> to test download and installation times.\n\n'
 	body += 'Available requirement sets:\n'
 	body += '\n'.join([' - ' + path.splitext(name)[0] for name in os.listdir(REQSET_DIR) if isreqset(name)])
-	return Response(body, mimetype='text/plain')
+	return res(body)
+
+@app.route('/timeout/<int:secs>')
+def test_timeout(secs):
+	sleep(secs)
+	return res('Slept')
 
 
 def reset_dir(path):
@@ -39,4 +48,4 @@ def runpip(reqset):
 	reset_dir(PIP_ROOT_DIR)
 	reqs_path = get_reqset_path(reqset)
 	pip_output = subprocess.check_output(['pip', 'install', '-r', reqs_path, '--root', PIP_ROOT_DIR])
-	return Response(pip_output, mimetype='text/plain')
+	return res(pip_output)
