bbc2e93edeeb8efafc16f190bff8882e9e3ebaf3
Dor Fire <dorfire@microsoft.com>
2019-07-15 17:42:46 -0700

    Added assertion

--- main.py
+++ main.py
@@ -103,8 +103,10 @@ def time_yarn(reqset):
 	# Install yarn if needed
 	if not cmd_exists('yarn'):
 		install_yarn_cmd = 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && apt-get update && apt-get install yarn'
-		body += '{}:\n'.format(install_yarn_cmd)
-		body += run(install_yarn_cmd).output + '\n\n'
+		body += 'Installing Yarn:\n'
+		install_yarn_run = run(install_yarn_cmd)
+		assert install_yarn_run.returncode == 0
+		body += install_yarn_run.output + '\n---\n'
 
 	app_path = path.join(NPM_REQSET_DIR, reqset)
 	# Always reset node_modules for yarn
